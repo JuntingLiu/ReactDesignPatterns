c34723efcfc98a2838b1dfa336c17bd72a6c34ac
junting <342766475@qq.com>
2022-03-20 18:27:41 +0800

    feat: uncontrolledOnboardingFlow

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import UncontrolledForm from './UncontrolledForm'
 import ControlledForm from './ControlledForm';
 import UncontrolledModal from './UncontrolledModal'
 import ControlledModal from './ControlledModal'
+import UncontrolledOnboardingFlow from './UncontrolledOnboardingFlow';
+
+
+const StepOne = ({ goToNext }) => (
+  <>
+    <h1>Step 1</h1>
+    <button onClick={() => goToNext({ name: 'Junting' })}>Next</button>
+  </>
+)
+const StepTwo = ({ goToNext }) => (
+  <>
+    <h1>Step 2</h1>
+    <button onClick={() => goToNext({ age: 18 })}>Next</button>
+  </>
+)
+const StepThree = ({ goToNext }) =>(
+  <>
+    <h1>Step 3</h1>
+    <button onClick={() => goToNext({ hairColor: 'black' })}>Next</button>
+  </>
+)
+const StepFour = ({ goToNext }) => (
+  <>
+    <h1>Step 4</h1>
+    <button onClick={() => goToNext({ hobby: 'Drink Coffee' })}>Next</button>
+  </>
+)
 
 function App() {
   const [shouldShow, setShouldShow] = useState(false)
@@ -21,6 +48,13 @@ function App() {
       {/* Controlled Modal */}
       <button onClick={() => handleDisplayModal(true)}>Show Controlled Modal</button>
       <ControlledModal shouldShow={shouldShow} displayModal={handleDisplayModal}/>
+      <h2>Uncontrolled Onboarding Flow</h2>
+      <UncontrolledOnboardingFlow onFinish={(data) => console.log('Finish:', data)}>
+        <StepOne />
+        <StepTwo />
+        <StepThree />
+        <StepFour />
+      </UncontrolledOnboardingFlow>
     </>
   );
 }
