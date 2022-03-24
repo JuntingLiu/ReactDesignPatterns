432464acd2650d1dc9f021b99d3bbb73dc192f3c
junting <342766475@qq.com>
2022-03-24 20:52:45 +0800

    feat: hocs-withUser|loading data with HOCs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import printProps from './printProps'
 import UserInfo from './UserInfo'
+import withUser from './withUser'
 
 const UserInfoWrapped = printProps(UserInfo)
+const UserInfoWithLoader = withUser(UserInfo, '123')
 
 function App() {
 
@@ -9,6 +11,7 @@ function App() {
     <>
       <h1>Higher-Order Components</h1>
       <UserInfoWrapped name={"Junting"} />
+      <UserInfoWithLoader />
     </>
   );
 }
