acb55d4b8c1bc857056cdab2a32f9f1b35f86c68
junting <342766475@qq.com>
2022-03-24 20:39:46 +0800

    feat: hocs-printProps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import printProps from './printProps'
+import UserInfo from './UserInfo'
 
+const UserInfoWrapped = printProps(UserInfo)
 
 function App() {
 
   return (
     <>
       <h1>Higher-Order Components</h1>
+      <UserInfoWrapped name={"Junting"} />
     </>
   );
 }
