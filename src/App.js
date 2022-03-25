9aa37369eef400d7f150b6d52d11282a5287e4f6
junting <342766475@qq.com>
2022-03-25 21:39:25 +0800

    feat: recursive component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import RecursiveComponent from './RecursiveComponent'
+
+const nestedObject = {
+	a: 1,
+	b: {
+		b1: 4,
+		b2: {
+			b23: 'Hello',
+		},
+		b3: {
+			b31: {
+				message: 'Hi',
+			},
+			b32: {
+				message: 'Hi',
+			}
+		}
+	},
+	c: {
+		c1: 2,
+		c2: 3,
+	}
+}
+
 function App() {
 
   return (
     <>
       <h1>Component composition</h1>
+      <br />
+      <RecursiveComponent data={nestedObject} />
     </>
   );
 }
---
+++ src/RecursiveComponent.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const isObject = x => typeof x === 'object' && x !== null && Array.isArray(x) === false
+
+const RecursiveComponent = ({data}) => {
+  if (!isObject(data)) {
+    return <li>{data}</li>
+  }
+  const pairs = Object.entries(data)
+
+  return (
+    <>
+      {pairs.map(([key, value]) => (
+        <li key={key}>
+          {key}:
+          <ul>
+            <RecursiveComponent data={value} />
+          </ul>
+        </li>
+      ))}
+    </>
+  )
+}
+
+export default RecursiveComponent
